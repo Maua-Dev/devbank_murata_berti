9a8854fffd66f08bcc2b97110598ecdc64f989ec
Vinícius de Oliveira Berti <vinicius.berti33@gmail.com>
2024-05-23 22:59:58 -0300

    "feat: add deposit"

---
+++ src/app/entities/deposit.py
@@ -0,0 +1,20 @@
+from typing import Dict
+from ..errors.entity_errors import ParamNotValidated
+
+class Deposit: 
+    def __init__(self, current_balance: float = None, timestamp: float = None):
+        if not self.validate_current_balance(current_balance):
+            raise ParamNotValidated("current_balance", "must be a float")
+        self.current_balance = current_balance
+
+        if not self.validate_timestamp(timestamp):
+            raise ParamNotValidated("timestamp", "must be a float")
+        self.timestamp = timestamp
+
+    @staticmethod
+    def validate_current_balance(current_balance) -> bool:
+        return isinstance(current_balance, float)
+    
+    @staticmethod
+    def validate_timestamp(timestamp) -> bool:
+        return isinstance(timestamp, float)

52c78f5b873d4bc40c19644a35c2b3a36f063c44
Vinícius de Oliveira Berti <vinicius.berti33@gmail.com>
2024-05-26 22:32:34 -0300

    "test: add transaction test"

--- tests/app/repo/test_transaction_repository_mock.py
+++ tests/app/repo/test_transaction_repository_mock.py
@@ -1,14 +1,58 @@
 import pytest
-# from src.app.entities.item import Item
-# from src.app.enums.item_type_enum import ItemTypeEnum
-from src.app.repo.user_repository_mock import UserRepositoryMocky
+from src.app.entities.transaction import Transaction
+from src.app.enums.transaction_type_enum import TransactionTypeEnum
+from src.app.repo.transaction_repository_mock import TransactionRepositoryMocky
 
-class Test_UserRepositoryMock:
-    def test_get_all_items(self):
-        repo = UserRepositoryMocky()
+class Test_TransactionRepositoryMock:
+    def test_get_all_transactions(self):
+        repo = TransactionRepositoryMocky()
+        
+        transactions = repo.get_all_transactions()
+        
+        expected_transactions = repo.transactions
+        
+        assert expected_transactions == transactions
 
-        items = repo.get_all_items()
+    def test_get_transaction(self):
+        repo = TransactionRepositoryMocky()
+        
+        transaction = repo.get_transaction(TransactionTypeEnum.DEPOSIT, 1000000000.0, 1000000000.0)
+        
+        expected_transaction = repo.transactions[0]
+        
+        assert expected_transaction == transaction
 
-        expected_item = repo.items
+        non_existent_transaction = repo.get_transaction(TransactionTypeEnum.WITHDRAWAL, 200.0, 1000000001.0)
+        assert non_existent_transaction is None
 
-        assert expected_item == items
+    def test_create_transaction(self):
+        repo = TransactionRepositoryMocky()
+        
+        new_transaction = Transaction(current_balance=500.0, timestamp=1000000001.0, value=500.0, type=TransactionTypeEnum.WITHDRAWAL)
+        created_transaction = repo.create_transaction(new_transaction)
+        
+        assert created_transaction == new_transaction
+        assert repo.get_transaction(TransactionTypeEnum.WITHDRAWAL, 500.0, 1000000001.0) == new_transaction
+
+    def test_delete_transaction(self):
+        repo = TransactionRepositoryMocky()
+        
+        deleted_transaction = repo.delete_transaction(TransactionTypeEnum.DEPOSIT, 1000000000.0, 1000000000.0)
+        
+        assert deleted_transaction.type == TransactionTypeEnum.DEPOSIT
+        assert repo.get_transaction(TransactionTypeEnum.DEPOSIT, 1000000000.0, 1000000000.0) is None
+
+        non_existent_transaction_deletion = repo.delete_transaction(TransactionTypeEnum.WITHDRAWAL, 200.0, 1000000001.0)
+        assert non_existent_transaction_deletion is None
+
+    def test_update_transaction(self):
+        repo = TransactionRepositoryMocky()
+        
+        new_transaction_data = Transaction(current_balance=2000000000.0, timestamp=1000000000.0, value=1000000000.0, type=TransactionTypeEnum.DEPOSIT)
+        updated_transaction = repo.update_transaction(TransactionTypeEnum.DEPOSIT, 1000000000.0, 1000000000.0, new_transaction_data)
+        
+        assert updated_transaction is not None
+        assert updated_transaction.current_balance == 2000000000.0
+        
+        non_existent_transaction_update = repo.update_transaction(TransactionTypeEnum.WITHDRAWAL, 200.0, 1000000001.0, new_transaction_data)
+        assert non_existent_transaction_update is None

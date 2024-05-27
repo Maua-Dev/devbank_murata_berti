3bb6bfb0b9078428828fb9d7a1724564e4a95ea7
Vinícius de Oliveira Berti <vinicius.berti33@gmail.com>
2024-05-26 22:26:57 -0300

    "feat: transaction interface and mock"

--- src/app/repo/transaction_repository_interface.py
+++ src/app/repo/transaction_repository_interface.py
@@ -1,15 +1,43 @@
 from abc import ABC, abstractmethod
-from typing import List, Optional, Tuple
-
-# from ..enums.item_type_enum import ItemTypeEnum
-
-from ..entities.transaction import Transaction
+from typing import List, Optional
+from src.app.enums.transaction_type_enum import TransactionTypeEnum
+from src.app.entities.transaction import Transaction
 
 class ITransactionRepository(ABC):
+    @abstractmethod
+    def get_all_transactions(self) -> List[Transaction]:
+        '''
+        Returns all the transactions in the database.
+        '''
+        pass
+    
+    @abstractmethod
+    def get_transaction(self, transaction_type: TransactionTypeEnum, value: float, timestamp: float) -> Optional[Transaction]:
+        '''
+        Returns the transaction with the given type, value, and timestamp.
+        If the transaction does not exist, returns None.
+        '''
+        pass
 
     @abstractmethod
-    def get_all_transaction(self) -> List[Transaction]:
-        """
-        Returns all the itens in the database 
-        """
+    def create_transaction(self, transaction: Transaction) -> Transaction:
+        '''
+        Creates a new transaction in the database and returns the created transaction.
+        '''
+        pass
+    
+    @abstractmethod
+    def delete_transaction(self, transaction_type: TransactionTypeEnum, value: float, timestamp: float) -> Optional[Transaction]:
+        '''
+        Deletes the transaction with the given type, value, and timestamp.
+        If the transaction does not exist, returns None.
+        '''
+        pass
+
+    @abstractmethod
+    def update_transaction(self, transaction_type: TransactionTypeEnum, value: float, timestamp: float, new_transaction: Transaction) -> Optional[Transaction]:
+        '''
+        Updates the transaction with the given type, value, and timestamp with the new transaction data.
+        If the transaction does not exist, returns None.
+        '''
         pass
--- src/app/repo/transaction_repository_mock.py
+++ src/app/repo/transaction_repository_mock.py
@@ -1,14 +1,36 @@
-from typing import Dict, Optional, List
-from ..entities.transaction import Transaction
-from ..repo.transaction_repository_interface import ITransactionRepository
+from typing import List, Optional
+from src.app.entities.transaction import Transaction
+from src.app.enums.transaction_type_enum import TransactionTypeEnum
+from src.app.repo.transaction_repository_interface import ITransactionRepository
 
 class TransactionRepositoryMocky(ITransactionRepository):
-    transaction: List[Transaction]
-
     def __init__(self):
-        self.transaction = [
-            Transaction(current_balance = 1000000000.0, timestamp= 1000000000.0, value= 1000000000.0, type= "deposit"),
+        self.transactions = [
+            Transaction(current_balance=1000000000.0, timestamp=1000000000.0, value=1000000000.0, type=TransactionTypeEnum.DEPOSIT),
         ]
 
-    def get_transaction(self) -> List[Transaction]:
-        return self.items
+    def get_all_transactions(self) -> List[Transaction]:
+        return self.transactions
+
+    def get_transaction(self, transaction_type: TransactionTypeEnum, value: float, timestamp: float) -> Optional[Transaction]:
+        for transaction in self.transactions:
+            if transaction.type == transaction_type and transaction.value == value and transaction.timestamp == timestamp:
+                return transaction
+        return None
+
+    def create_transaction(self, transaction: Transaction) -> Transaction:
+        self.transactions.append(transaction)
+        return transaction
+
+    def delete_transaction(self, transaction_type: TransactionTypeEnum, value: float, timestamp: float) -> Optional[Transaction]:
+        for idx, transaction in enumerate(self.transactions):
+            if transaction.type == transaction_type and transaction.value == value and transaction.timestamp == timestamp:
+                return self.transactions.pop(idx)
+        return None
+
+    def update_transaction(self, transaction_type: TransactionTypeEnum, value: float, timestamp: float, new_transaction: Transaction) -> Optional[Transaction]:
+        for idx, transaction in enumerate(self.transactions):
+            if transaction.type == transaction_type and transaction.value == value and transaction.timestamp == timestamp:
+                self.transactions[idx] = new_transaction
+                return new_transaction
+        return None
